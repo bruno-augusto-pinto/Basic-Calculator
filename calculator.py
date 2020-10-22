0ecb3682ffe1a7c6beb8ad221474f1f54798735c
bruno-augusto-pinto <brunoaugustopinto@hotmail.com>
2020-10-21 21:17:19 -0300

    Fix: error in non float input

--- calculator.py
+++ calculator.py
@@ -28,18 +28,23 @@ while True:
     if op in ('0', '1', '2', '3', '4'):
         if op == '0':
             break
-        num1 = float(input('Digite o primeiro valor: '))
-        num2 = float(input('Digite o segundo valor: '))
-        if op == '1':
-            res = adicao(num1, num2)
-        elif op == '2':
-            res = subtracao(num1, num2)
-        elif op == '3':
-            res = multiplicacao(num1, num2)
-        elif op == '4':
-            res = divisao(num1, num2)
-        print('Resultado: ', res)
-        print(" ")
+        try:
+            num1 = float(input('Digite o primeiro valor: '))
+            num2 = float(input('Digite o segundo valor: '))
+            if op == '1':
+                res = adicao(num1, num2)
+            elif op == '2':
+                res = subtracao(num1, num2)
+            elif op == '3':
+                res = multiplicacao(num1, num2)
+            elif op == '4':
+                res = divisao(num1, num2)
+            print('Resultado: ', res)
+            print(" ")
+        except:
+            print("Você digitou um valor inválido, tente novamente")
+            print('')
+            continue
     else:
         print("Você digitou um valor inválido, tente novamente")
         print('')
